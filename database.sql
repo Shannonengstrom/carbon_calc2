ca8e595990d4e2f0442271b046d84a4f1318d8a6
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-11 22:10:38 -0500

    save database sql queries

--- database.sql
+++ database.sql
@@ -1,5 +1,36 @@
 CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
-);
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(80) UNIQUE NOT NULL,
+  "password" VARCHAR(1000) NOT NULL
+); 
+
+CREATE TABLE co2_emis (
+  "id" SERIAL PRIMARY KEY,
+  "mode" VARCHAR(80) NOT NULL,
+  "multiplier" DECIMAL
+); 
+
+CREATE TABLE logs (
+  "id" SERIAL PRIMARY KEY,
+  "co2_emis_id" INT REFERENCES co2_emis,
+  "destination" VARCHAR(80) NOT NULL,
+  "date" date NOT NULL,
+  "miles" DECIMAL NOT NULL,
+  "notes" VARCHAR(200),
+  "total_emis" DECIMAL,
+  "total_saved" DECIMAL,
+  "person_id" INT REFERENCES person
+); 
+
+
+INSERT INTO person ("username", "password")
+VALUES ('test1', 'password123'), ('test2', 'password123'), ('test3', 'password123'), ('test4', 'password123'),
+('test5', 'password123'), ('test6', 'password123'), ('test7', 'password123');
+
+INSERT INTO co2_emis ("mode", "multiplier")
+VALUES ('bus', 10), ('train', 100), ('car', 1000), ('walk', 2), ('bike', 2);
+
+INSERT INTO logs ("co2_emis_id", "destination", "date", "miles", "notes", "total_emis", "total_saved", "person_id")
+VALUES (1, 'work', '09-15-1991', 5.3, '', 10, 5, 1);
+
+
