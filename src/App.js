ce62a3f79d935b3f3fcf0154ff806cd6cb369699
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-11 21:29:03 -0500

    add base folder structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+// react
 import React from 'react';
 import {
   HashRouter as Router,
@@ -6,17 +8,43 @@ import {
   Switch,
 } from 'react-router-dom';
 
+// headers
+import LoginHeader from './components/Headers/LoginHeader';
 import MainHeader from './components/Headers/MainHeader';
+
+// main pages
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import UserPage from './components/UserPage/UserPage';
 import InfoPage from './components/InfoPage/InfoPage';
 
+// log pages
+import BusLogPage from './components/LogPages/BusLogPage';
+import TrainLogPage from './components/LogPages/TrainLogPage';
+import WalkLogPage from './components/LogPages/WalkLogPage';
+import BikeLogPage from './components/LogPages/BikeLogPage';
+import CarLogPage from './components/LogPages/CarLogPage';
+import LogForm from './components/LogPages/LogForm';
+
+// results pages
+import AllResultsPage from './components/ResultsPages/AllResultsPage';
+import DailyResultsPage from './components/ResultsPages/DailyResultsPage';
+import MonthlyResultsPage from './components/ResultsPages/MonthlyResultsPage';
+import YearlyResultsPage from './components/ResultsPages/YearlyResultsPage';
+import ModalResultsPage from './components/ResultsPages/ModalResultsPage';
+
+// error pages
+import Error404Page from './components/ErrorPages/Error404Page';
+import Forbidden403Page from './components/ErrorPages/Forbidden403Page';
+
+
+
+//styles
 import './styles/main.css';
 
 const App = () => (
   <div>
-    <Header title="Project Base" />
+    <MainHeader title="Project Base" />
     <Router>
       <Switch>
         <Redirect exact from="/" to="/home" />
