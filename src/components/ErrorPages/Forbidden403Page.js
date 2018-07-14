16b52e662d915c74fd053e87b872e5cbb5f231da
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-14 15:13:05 -0500

    format error pages

---
+++ src/components/ErrorPages/Forbidden403Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error404Page = (
+  <div className="errorPage">
+    <h1>Forbidden 403 :  you must login to access this page.</h1>
+  </div>
+);
+
+export default Error404Page;
+
+
+
