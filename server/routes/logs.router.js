4a5e2d8e5775b9adb742b469d901b2ba16601eca
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-17 16:17:00 -0500

    get carbon id

--- server/routes/logs.router.js
+++ server/routes/logs.router.js
@@ -8,18 +8,18 @@ router.post('/', (req, res) => {
     console.log('in router.post', req.body);
     const id = req.user.id;
     // const co2_emis_id = req.body. 
-    const newLog = req.body.body.newInput; 
-    const co2 = req.body.body;
+    const newInput = req.body.body.newInput; 
+    const newCo2 = req.body;
     const queryText =  `INSERT INTO logs ("co2_emis_id", "destination", "date", "miles", "notes", "total_emis", "total_saved", "person_id")
                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
     const queryValues = [
-        newLog.co2_emis_id,
-        newLog.destination, 
-        newLog.date, 
-        newLog.miles, 
-        newLog.notes,
-        newLog.total_emis, 
-        newLog.total_saved,
+        newCo2.co2_emis_id,
+        newInput.destination, 
+        newInput.date, 
+        newInput.miles, 
+        newInput.notes,
+        newInput.total_emis, 
+        newInput.total_saved,
         id
     ];
     pool.query(queryText, queryValues)
