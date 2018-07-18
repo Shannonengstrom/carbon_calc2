c13f3584667100e928c4a05923de22929a831040
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-18 15:15:30 -0500

    add train log

--- src/components/LogPages/TrainLogPage.js
+++ src/components/LogPages/TrainLogPage.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import LogForm from './LogForm';
 import Nav from '../../components/Nav/Nav';
 
-const TrainLogPage = props => (
-  <div className="trainLogPage">
-    <Nav />
-    <h2>new log : train</h2>
-    <LogForm />
-  </div>
-);
+const mapReduxToProps = (reduxStore) => ({
+  reduxStore
+});
 
-export default TrainLogPage;
+class TrainLogPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaults: {
+        mode: 'train',
+        co2_emis: 6
+      }
+    }
+  }
+
+
+  sendMultToRedux = () => {
+    const body = this.state.defaults;
+    console.log(body);
+    const action = {type: 'ADD_DEFAULTS', payload: body};
+    this.props.dispatch(action);
+    console.log(action);
+  }
+
+  componentDidMount () {
+    this.sendMultToRedux();
+  } 
+
+  render() {
+    return (
+      <div className="trainLogPage">
+        <Nav />
+        <h2>new log : train</h2>
+        <pre>{JSON.stringify(this.state)}</pre>
+        <LogForm co2={this.state}/>
+      </div>
+    );
+  }
+}
+
+export default connect(mapReduxToProps)(TrainLogPage);
+
