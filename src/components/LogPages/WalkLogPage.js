79c1bfb0a1e58d8f26ccb9a537dced521154bdb6
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-18 15:18:05 -0500

    walk log page

--- src/components/LogPages/WalkLogPage.js
+++ src/components/LogPages/WalkLogPage.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import LogForm from './LogForm';
 import Nav from '../../components/Nav/Nav';
 
-const WalkLogPage = props => (
-  <div className="walkLogPage">
-    <Nav />
-    <h2>new log : walk</h2>
-    <LogForm />
-  </div>
-);
+const mapReduxToProps = (reduxStore) => ({
+  reduxStore
+});
 
-export default WalkLogPage;
+class WalkLogPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaults: {
+        mode: 'walk',
+        co2_emis: 0
+      }
+    }
+  }
+
+
+  sendMultToRedux = () => {
+    const body = this.state.defaults;
+    console.log(body);
+    const action = {type: 'ADD_DEFAULTS', payload: body};
+    this.props.dispatch(action);
+    console.log(action);
+  }
+
+  componentDidMount () {
+    this.sendMultToRedux();
+  } 
+
+  render() {
+    return (
+      <div className="walkLogPage">
+        <Nav />
+        <h2>new log : walk</h2>
+        <pre>{JSON.stringify(this.state)}</pre>
+        <LogForm co2={this.state}/>
+      </div>
+    );
+  }
+}
+
+export default connect(mapReduxToProps)(WalkLogPage);
+
