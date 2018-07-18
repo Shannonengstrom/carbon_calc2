b59c7f46cafdc4f637505b62a26d1cd7c0db16a9
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-18 15:20:02 -0500

    update database sql queries

--- database.sql
+++ database.sql
@@ -6,6 +6,7 @@ CREATE TABLE person (
 
 CREATE TABLE logs (
   "id" SERIAL PRIMARY KEY,
+  "mode" VARCHAR(20),
   "co2_emis" DECIMAL NOT NULL,
   "destination" VARCHAR(80) NOT NULL,
   "date" date NOT NULL,
@@ -20,7 +21,7 @@ INSERT INTO person ("username", "password")
 VALUES ('test1', 'password123'), ('test2', 'password123'), ('test3', 'password123'), ('test4', 'password123'),
 ('test5', 'password123'), ('test6', 'password123'), ('test7', 'password123');
 
-INSERT INTO logs ("co2_emis", "destination", "date", "miles", "notes", "total_emis", "person_id")
-VALUES (30, 'work', '09-15-1991', 5.3, '', 10, 1);
+INSERT INTO logs ("mode", "co2_emis", "destination", "date", "miles", "notes", "total_emis", "person_id")
+VALUES ('bike', 30, 'work', '09-15-1991', 5.3, '', 10, 1);
 
 
