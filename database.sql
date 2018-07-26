546ddcd4ca565bb34d6283d404847a5c26d7272b
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-25 23:15:23 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+-- Setup Instructions: 
+-- create `person` and `logs` tables
+
 CREATE TABLE person (
   "id" SERIAL PRIMARY KEY,
   "username" VARCHAR(80) UNIQUE NOT NULL,
@@ -15,18 +18,3 @@ CREATE TABLE logs (
   "total_emis" DECIMAL,
   "person_id" INT REFERENCES person
 ); 
-
-
-INSERT INTO person ("username", "password")
-VALUES ('test1', 'password123'), ('test2', 'password123'), ('test3', 'password123'), ('test4', 'password123'),
-('test5', 'password123'), ('test6', 'password123'), ('test7', 'password123');
-
-INSERT INTO logs ("mode", "co2_emis", "destination", "date", "miles", "notes", "total_emis", "person_id")
-VALUES ('bike', 30, 'work', '09-15-1991', 5.3, '', 10, 1);
-
-SELECT SUM(total_emis)
-FROM logs;
-
-UPDATE logs
-SET mode = $1, co2_emis = $2, destination = $3, date = $4, miles = $5, notes = $6
-WHERE id = ${id};
